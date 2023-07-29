900054d8d0ff6b73306f0f1b161c5488632b59c6
jason.schaeffer <schaeffer.jason@gmail.com>
2023-07-29 12:34:37 -0400

    Week 4 Assignment

--- nucampsite/screens/LoginScreen.js
+++ nucampsite/screens/LoginScreen.js
@@ -6,6 +6,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as ImagePicker from "expo-image-picker";
 import { baseUrl } from "../shared/baseUrl";
 import logo from "../assets/images/logo.png";
+import { manipulateAsync, SaveFormat } from "expo-image-manipulator";
+import * as MediaLibrary from "expo-media-library";
 
 const LoginTab = ({ navigation }) => {
     const [username, setUsername] = useState("");
@@ -148,9 +150,41 @@ const RegisterTab = () => {
             });
             if (capturedImage.assets) {
                 console.log(capturedImage.assets[0]);
-                setImageUrl(capturedImage.assets[0].uri);
+                MediaLibrary.saveToLibraryAsync(capturedImage.assets[0].uri);
+                processImage(capturedImage.assets[0].uri);
             }
         }
+    };
+
+    const getImageFromGallery = async () => {
+        const mediaLibraryPermissions =
+            await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (mediaLibraryPermissions.status === "granted") {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1],
+            });
+            if (capturedImage.assets) {
+                console.log(capturedImage.assets[0]);
+                processImage(capturedImage.assets[0].uri);
+            }
+        }
+    };
+
+    const processImage = async (imgUri) => {
+        const processedImage = await manipulateAsync(
+            imgUri,
+            [
+                {
+                    resize: { height: 400, width: 400 },
+                },
+            ],
+            { format: SaveFormat.PNG }
+        );
+        console.log(processedImage);
+        MediaLibrary.saveToLibraryAsync(processedImage.uri);
+        setImageUrl(processedImage.uri);
     };
 
     return (
@@ -163,6 +197,7 @@ const RegisterTab = () => {
                         style={styles.image}
                     />
                     <Button title="Camera" onPress={getImageFromCamera} />
+                    <Button title="Gallery" onPress={getImageFromGallery} />
                 </View>
                 <Input
                     placeholder="Username"
